315fc753dd728f1dadd95bbe41bce05a5da39589
Peyton <41251008+pxgraham@users.noreply.github.com>
2019-11-14 19:57:37 -0700

    created stripe data

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
+const stripe = require('stripe')(process.env.stripe_key);
 
-var app = express();
+const app = express();
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,6 +12,18 @@ app.use("/public", express.static(__dirname + "/public"))
 
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.post("/charge", function(req, res) {
+    const token = req.body.stripeToken;
+    (async () => {
+        const charge = await stripe.charges.create({
+          amount: 1,
+          currency: 'usd',
+          description: 'Example charge',
+          source: token,
+        });
+    })();
 });
 
 app.listen(PORT, function() {
